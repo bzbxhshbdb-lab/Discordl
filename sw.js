a86be3f034b34d6dbd6081eb2f1108a49d28c60d
bzbxhshbdb-lab <bzbxhshbdb@gmail.com>
2026-01-20 16:52:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,28 @@
-const CACHE_NAME = "discord-pwa-cache-v2";
+const CACHE_NAME = "discord-pwa-cache-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.png"
+];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./manifest.json"
-      ]);
-    })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
