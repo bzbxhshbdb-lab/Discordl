c6b77076f355a41aaaafc9fa11b29922ee6750d7
bzbxhshbdb-lab <bzbxhshbdb@gmail.com>
2026-01-20 16:38:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,39 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("zxiter-cache").then(cache => {
+const CACHE_NAME = "discord-pwa-cache-v2";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
         "./style.css",
-        "./script.js"
+        "./script.js",
+        "./manifest.json"
       ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
